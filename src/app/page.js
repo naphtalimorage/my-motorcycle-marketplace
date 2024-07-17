32ad783006e74a6af85c9e73a93078b93f4bda32
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-17 18:24:13 +0300

    remove unnecessary styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="relative top-34 ml-2 text-5xl font-bold font-sans">
+      <div className="relative  ml-2 text-5xl font-bold font-sans">
         <h1>
           Buy and Sell your
           <br />
@@ -11,16 +12,20 @@ export default function Home() {
           <br /> and Safe Way
         </h1>
       </div>
-      <div className="absolute left-2/4 bg-red-300">
-        <Image src={"/boxer.jpeg"} width={800} height={800} alt="boxer 150" />
+      <div className="ml-96">
+        <Image src={"/boxer.jpeg"} width={800} height={600} alt="boxer 150"/>
       </div>
-      <div className="space-x-4 relative mt-96 ml-2">
-        <button className="bg-emerald-400 rounded-lg py-4 px-4 text-xl">
-          Buy A Motorcycle
-        </button>
-        <button className="bg-emerald-400 rounded-lg py-4 px-4 text-xl">
-          Sell Your Motorcycle
-        </button>
+      <div className="space-x-4  ml-2">
+        <Link href="/components/buymotorcycle">
+          <button className="bg-emerald-400 rounded-lg py-4 px-4 text-xl">
+            Buy A Motorcycle
+          </button>
+        </Link>
+        <Link href="/components/sellmotorcycle">
+          <button className="bg-emerald-400 rounded-lg py-4 px-4 text-xl">
+            Sell Your Motorcycle
+          </button>
+        </Link>
       </div>
     </>
   );
